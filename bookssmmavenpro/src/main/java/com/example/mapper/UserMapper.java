b822037cbe5b35e5ac1cdf1ab29a9b1103a4b7ae
yudongyue <yudongyue@daqo.com>
2020-11-24 15:35:11 +0800

    ssm-maven-图书管理项目

---
+++ bookssmmavenpro/src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.example.mapper;
+
+import com.example.model.User;
+
+public interface UserMapper {
+    User getUserByUser(User user);
+}
---
+++ bookssmmavenpro/src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.service.impl;
+
+import com.example.mapper.UserMapper;
+import com.example.model.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper userMapper;
+    @Override
+    public User getUserByUser(User user) {
+        return userMapper.getUserByUser(user);
+    }
+}
